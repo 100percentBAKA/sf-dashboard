e49cd69de16653bfa54bbd25063837fa2a18dc27
100percentBAKA <1rn21cs011.adarshgs@gmail.com>
2024-02-22 22:04:39 +0530

    add layout

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -7,9 +7,22 @@ import { Outlet } from "react-router-dom";
 
 const Root = () => {
   return (
-    <div>
-      <div>This is the Root Page</div>
-      <Outlet />
+    <div className="w-screen h-screen flex">
+      {/* fixed sidebar */}
+      <div className="hidden lg:flex lg:h-[100%] lg:w-[300px] xl:w-[330px] bg-green-500">
+        Sidebar
+      </div>
+
+      {/* Main area */}
+      <div className="flex-1 flex flex-col bg-red-500 p-6">
+        {/* fixed navbar */}
+        <nav className="mb-14 p-4 bg-yellow-500">Navbar</nav>
+
+        {/* main working area */}
+        <main className="bg-blue-500 h-full">
+          <Outlet />
+        </main>
+      </div>
     </div>
   );
 };
