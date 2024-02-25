afee071c0a903279823acc310a45351aca3be2af
100percentBAKA <1rn21cs011.adarshgs@gmail.com>
2024-02-25 17:46:01 +0530

    fix layout

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -10,13 +10,12 @@ import Navbar from "../components/Navbar";
 const Root = () => {
   return (
     <div className="w-screen h-screen flex">
-      {/* Fixed sidebar */}
-      <div className="h-full fixed left-0 top-0 bg-green-500 w-[250px] xl:w-[330px]">
+      <div className="hidden md:block h-full fixed left-0 top-0 bg-green-500 w-[250px] xl:w-[330px]">
         <Sidebar />
       </div>
 
       {/* Main area */}
-      <div className="flex-1 ml-[250px] xl:ml-[330px] p-6">
+      <div className="flex-1 md:ml-[250px] xl:ml-[330px] p-6">
         <Navbar />
 
         {/* Main working area */}
