19621c9934f94fb6d9306bc7dba26fc9712570cd
100percentBAKA <1rn21cs011.adarshgs@gmail.com>
2024-03-02 10:35:40 +0530

    update routing

--- src/Router.js
+++ src/Router.js
@@ -1,49 +1,57 @@
-import { createBrowserRouter } from "react-router-dom"
-import Root from "./pages/Root"
-import Dashboard from "./pages/Dashboard"
-import Login from "./pages/Login"
-import ForgetPasswordRoot from "./pages/forget/ForgetPasswordRoot"
-import ForgetForm from "./pages/forget/ForgetForm"
-import OtpForm from "./pages/forget/OtpForm"
-import ResetForm from "./pages/forget/ResetForm"
+import { createBrowserRouter } from "react-router-dom";
+import Root from "./pages/Root";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import ForgetPasswordRoot from "./pages/forget/ForgetPasswordRoot";
+import ForgetForm from "./pages/forget/ForgetForm";
+import OtpForm from "./pages/forget/OtpForm";
+import ResetForm from "./pages/forget/ResetForm";
+import FileUpload from "./pages/FileUpload";
+import ExcelUpload from "./pages/ExcelUpload";
 
-const router = createBrowserRouter(
-    [
-        {
-            path: "/",
-            element: <Root />,
-            children: [
-                {
-                    path: "/",
-                    element: <Dashboard />,
-                    index: true
-                }
-            ]
-        },
-        {
-            path: "/login",
-            element: <Login />
-        },
-        {
-            path: "/forget",
-            element: <ForgetPasswordRoot />,
-            children: [
-                {
-                    path: "/forget",
-                    element: <ForgetForm />,
-                    index: true
-                },
-                {
-                    path: "/forget/otp",
-                    element: <OtpForm />
-                },
-                {
-                    path: "/forget/reset",
-                    element: <ResetForm />
-                }
-            ]
-        }
-    ]
-)
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Login />,
+    },
+    {
+        path: "/app",
+        element: <Root />,
+        children: [
+            {
+                path: "dashboard",
+                element: <Dashboard />,
+                index: true,
+            },
+            {
+                path: "file-upload",
+                element: <FileUpload />,
+            },
+            {
+                path: "excel-upload",
+                element: <ExcelUpload />,
+            },
+        ],
+    },
+    {
+        path: "/forget",
+        element: <ForgetPasswordRoot />,
+        children: [
+            {
+                path: "/forget",
+                element: <ForgetForm />,
+                index: true,
+            },
+            {
+                path: "otp",
+                element: <OtpForm />,
+            },
+            {
+                path: "reset",
+                element: <ResetForm />,
+            },
+        ],
+    },
+]);
 
 export default router;
