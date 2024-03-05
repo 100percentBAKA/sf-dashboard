acb9b0dfa62ef961289d6d589b54d0325c3034a9
100percentBAKA <1rn21cs011.adarshgs@gmail.com>
2024-03-05 11:16:43 +0530

    move categories data into data dir

---
+++ src/data/categories.js
@@ -0,0 +1,7 @@
+const tableData = [
+    { id: 1, name: "image1", imageUrl: "" },
+    { id: 2, name: "image2", imageUrl: "" },
+    { id: 3, name: "image3", imageUrl: "" },
+];
+
+export default tableData;
