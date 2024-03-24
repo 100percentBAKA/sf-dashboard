1550b4d8533a42c61f11c36ebbdf04c0c098f666
100percentBAKA <1rn21cs011.adarshgs@gmail.com>
2024-03-24 14:12:44 +0530

    add QueryClientProvider and Toaster

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { Toaster } from "react-hot-toast";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <Toaster position="top-center" toastOptions={{ duration: 1500 }} />
+    </QueryClientProvider>
+  </React.StrictMode>
+);
