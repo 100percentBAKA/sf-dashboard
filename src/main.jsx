d2d2e0b5a1672a2c5bb5c51407c5832d3d7dc960
100percentBAKA <1rn21cs011.adarshgs@gmail.com>
2024-03-28 15:29:31 +0530

    limit retries to 2

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,19 @@ import "./index.css";
 import { Toaster } from "react-hot-toast";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 2,
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
-      <Toaster position="top-center" toastOptions={{ duration: 3000 }} />
+      <Toaster position="top-center" toastOptions={{ duration: 2000 }} />
     </QueryClientProvider>
   </React.StrictMode>
 );
