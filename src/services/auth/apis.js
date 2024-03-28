e5e55b800d8ec00c05d71fc33041d8de57db9ec2
100percentBAKA <1rn21cs011.adarshgs@gmail.com>
2024-03-28 15:44:10 +0530

    seperate axios instances

--- src/services/auth/apis.js
+++ src/services/auth/apis.js
@@ -1,4 +1,7 @@
-import { axiosInstance } from "../apis_head"
+import axios from "axios"
+
+export const BASE_URL = "http://13.233.65.5:8000/api/v2"
+export const axiosInstance = axios.create({ baseURL: BASE_URL })
 
 //! AUTH APIS
 
