ae806a17320ad24ddefcf591bdd238fde76daf63
100percentBAKA <1rn21cs011.adarshgs@gmail.com>
2024-03-28 15:43:42 +0530

    add global states to store

--- src/stores/store.js
+++ src/stores/store.js
@@ -2,7 +2,11 @@ import { create } from "zustand";
 
 export const useStore = create((set) => ({
     username: "",
-    phoneNo: "",
+    phoneNo: "+919876543210", // * fallback phone no
+    currentSubCat: "",
+    currentSubSubCat: "",
     setUsername: (username) => set({ username }),
-    setPhoneNo: (phoneNo) => set({ phoneNo })
+    setPhoneNo: (phoneNo) => set({ phoneNo }),
+    setCurrentSubCat: (currentSubCat) => set({ currentSubCat }),
+    setCurrentSubSubCat: (currentSubSubCat) => set({ currentSubSubCat })
 }))
