171aa985fadf44983c0f4238d0513c0417af3deb
AD4R5HG5 <117382591+100percentBAKA@users.noreply.github.com>
2024-05-12 19:01:07 +0530

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -76,7 +76,7 @@ const Login = () => {
         onSubmit={formik.handleSubmit}
         className="bg-white p-6 lg:p-10 xl:p-12 flex flex-col gap-6 rounded-[10px]"
       >
-        <div className="text-2xl font-semibold">Welcome</div>
+        <div className="text-2xl font-semibold">Welcome User</div>
 
         {/* username */}
         <div className="flex flex-col gap-2">
